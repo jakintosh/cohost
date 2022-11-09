bd38adc606cced2e262573f533192745aaef2bd2
Jak Tiano <jak33@mac.com>
2022-11-09 18:49:41 -0500

    started adding DMA features, cleaned up some scaffolding

--- src/core.rs
+++ src/core.rs
@@ -36,6 +36,8 @@ enum Instruction {
     Address,
     Store,
     Load,
+    DMASet,
+    DMACheck,
     DropData,
     DropSwap,
     DropReturn,
@@ -98,9 +100,11 @@ impl From<u8> for Instruction {
             0x16 => Self::Address,
             0x17 => Self::Store,
             0x18 => Self::Load,
-            0x19 => Self::DropData,
-            0x1a => Self::DropSwap,
-            0x1b => Self::DropReturn,
+            0x19 => Self::DMASet,
+            0x1a => Self::DMACheck,
+            0x1b => Self::DropData,
+            0x1c => Self::DropSwap,
+            0x1d => Self::DropReturn,
 
             // logic and arithmetic
             0x20 => Self::Add,
@@ -123,16 +127,35 @@ impl From<u8> for Instruction {
     }
 }
 
+pub struct DMA {
+    pub status_reg: u8,
+    pub address: u32,
+    pub length: u32,
+}
+impl DMA {
+    pub const REQUEST_BIT: u8 = 0x80;
+    pub const RESPONSE_BIT: u8 = 0x40;
+
+    pub fn new() -> DMA {
+        DMA {
+            status_reg: 0,
+            address: 0,
+            length: 0,
+        }
+    }
+}
+
 pub(crate) struct CPU {
-    program_counter: u32,
-    memory_address: u32,
+    pub program_counter: u32,
+    pub memory_address: u32,
 
-    hold_reg: Register64,
-    data_st: Stack,
-    swap_st: Stack,
-    return_st: Stack,
+    pub hold_reg: Register64,
+    pub data_st: Stack,
+    pub swap_st: Stack,
+    pub return_st: Stack,
 
-    memory: [u8; 65_536],
+    pub memory: [u8; 65_536],
+    pub dma_controller: DMA,
 }
 impl CPU {
     pub fn new() -> CPU {
@@ -146,155 +169,170 @@ impl CPU {
             return_st: Stack::new(),
 
             memory: [0; 65_536],
+            dma_controller: DMA::new(),
         }
+    }
+    pub fn execute(&mut self) {
+        let instruction: u8 = self.memory[self.program_counter as usize];
+        let len = CPU::parse_bytes(instruction);
+        match instruction.into() {
+            // stack movement
+            Instruction::CopyDataToData => self.data_st.duplicate(len),
+            Instruction::CopyDataToSwap => self.swap_st.push(self.data_st.top(len)),
+            Instruction::CopyDataToReturn => self.return_st.push(self.data_st.top(len)),
+            Instruction::CopyDataToHold => self.hold_reg.push(self.data_st.top(len)),
+            Instruction::CopySwapToData => self.data_st.push(self.swap_st.top(len)),
+            Instruction::CopySwapToSwap => self.swap_st.duplicate(len),
+            Instruction::CopySwapToReturn => self.return_st.push(self.swap_st.top(len)),
+            Instruction::CopySwapToHold => self.hold_reg.push(self.swap_st.top(len)),
+            Instruction::CopyReturnToData => self.data_st.push(self.return_st.top(len)),
+            Instruction::CopyReturnToSwap => self.swap_st.push(self.return_st.top(len)),
+            Instruction::CopyReturnToReturn => self.return_st.duplicate(len),
+            Instruction::CopyReturnToHold => self.hold_reg.push(self.return_st.top(len)),
+            Instruction::CopyHoldToData => self.data_st.push(self.hold_reg.top(len)),
+            Instruction::CopyHoldToSwap => self.swap_st.push(self.hold_reg.top(len)),
+            Instruction::CopyHoldToReturn => self.return_st.push(self.hold_reg.top(len)),
+            Instruction::Literal => {
+                let lit_range = self.get_lit_range(len);
+                self.data_st.push(&self.memory[lit_range]);
+                self.program_counter += 1 + len as u32;
+                return; // avoid default PC increment
+            }
+
+            // branching
+            Instruction::Jump {
+                conditional,
+                relative,
+            } => {
+                if conditional {
+                    let condition = self.data_st.top_byte();
+                    self.data_st.drop(1);
+                    if condition == STACK_FALSE {
+                        return; // don't execute the jump
+                    };
+                }
+
+                let value = self.pop_operand32(len);
+                self.program_counter = match relative {
+                    true => self.program_counter + value,
+                    false => value,
+                };
+
+                return; // avoid default PC increment
+            }
+            Instruction::Call => {
+                self.return_st.push(&self.program_counter.to_le_bytes());
+                self.program_counter = self.pop_operand32(len);
+                return; // avoid default PC increment
+            }
+            Instruction::Return => {
+                self.program_counter = CPU::le_slice_to_u32(self.return_st.top(len));
+                self.return_st.drop(len);
+                return; // avoid default PC increment
+            }
+
+            // accessing memory
+            Instruction::Address => {
+                self.memory_address = self.pop_operand32(len);
+            }
+            Instruction::Store => {
+                let data = self.data_st.top(len);
+                let range = self.memory_address as usize..self.memory_address as usize + len;
+                self.memory[range].copy_from_slice(data);
+            }
+            Instruction::Load => {
+                let range = self.memory_address as usize..self.memory_address as usize + len;
+                let data = &self.memory[range];
+                self.data_st.push(data);
+            }
+
+            // working with DMA
+            Instruction::DMASet => {
+                let flag = self.pop_operand8();
+                let (address, length) = self.pop_operands32(len);
+                self.dma_controller.status_reg |= flag;
+                self.dma_controller.address = address;
+                self.dma_controller.length = length;
+            }
+            Instruction::DMACheck => {
+                let flag = self.pop_operand8();
+                let flag_set = (self.dma_controller.status_reg & flag) != 0;
+                self.push_result_bool(flag_set);
+            }
+
+            // dropping stacks
+            Instruction::DropData => self.data_st.drop(len),
+            Instruction::DropSwap => self.swap_st.drop(len),
+            Instruction::DropReturn => self.return_st.drop(len),
+
+            // arithmetic
+            Instruction::Add => {
+                let (lhs, rhs) = self.pop_operands64(len);
+                self.push_result64(len, lhs + rhs);
+            }
+            Instruction::Subtract => {
+                let (lhs, rhs) = self.pop_operands64(len);
+                self.push_result64(len, lhs - rhs);
+            }
+            Instruction::Multiply => {
+                let (lhs, rhs) = self.pop_operands64(len);
+                self.push_result64(len, lhs * rhs);
+            }
+            Instruction::Divide => {
+                let (lhs, rhs) = self.pop_operands64(len);
+                self.push_result64(len, lhs / rhs);
+            }
+
+            // bitwise logic
+            Instruction::And => {
+                let (lhs, rhs) = self.pop_operands64(len);
+                self.push_result64(len, lhs & rhs);
+            }
+            Instruction::Or => {
+                let (lhs, rhs) = self.pop_operands64(len);
+                self.push_result64(len, lhs | rhs);
+            }
+            Instruction::Xor => {
+                let (lhs, rhs) = self.pop_operands64(len);
+                self.push_result64(len, lhs ^ rhs);
+            }
+            Instruction::Not => {
+                let operand = self.pop_operand64(len);
+                self.push_result64(len, !operand);
+            }
+            Instruction::ShiftL => {
+                let (shift, operand) = self.pop_operands64(len);
+                self.push_result64(len, operand << shift);
+            }
+            Instruction::ShiftR => {
+                let (shift, operand) = self.pop_operands64(len);
+                self.push_result64(len, operand >> shift);
+            }
+
+            // comparisons
+            Instruction::Greater => {
+                let (lhs, rhs) = self.pop_operands64(len);
+                self.push_result_bool(lhs > rhs);
+            }
+            Instruction::Less => {
+                let (lhs, rhs) = self.pop_operands64(len);
+                self.push_result_bool(lhs < rhs);
+            }
+            Instruction::Equal => {
+                let (lhs, rhs) = self.pop_operands64(len);
+                self.push_result_bool(lhs == rhs);
+            }
+            Instruction::NotEqual => {
+                let (lhs, rhs) = self.pop_operands64(len);
+                self.push_result_bool(lhs != rhs);
+            }
+        }
+
+        self.program_counter += 1;
     }
     pub fn run(&mut self) {
         loop {
-            let instruction: u8 = self.memory[self.program_counter as usize];
-            let len = CPU::parse_bytes(instruction);
-            match instruction.into() {
-                // stack movement
-                Instruction::CopyDataToData => self.data_st.duplicate(len),
-                Instruction::CopyDataToSwap => self.swap_st.push(self.data_st.top(len)),
-                Instruction::CopyDataToReturn => self.return_st.push(self.data_st.top(len)),
-                Instruction::CopyDataToHold => self.hold_reg.push(self.data_st.top(len)),
-                Instruction::CopySwapToData => self.data_st.push(self.swap_st.top(len)),
-                Instruction::CopySwapToSwap => self.swap_st.duplicate(len),
-                Instruction::CopySwapToReturn => self.return_st.push(self.swap_st.top(len)),
-                Instruction::CopySwapToHold => self.hold_reg.push(self.swap_st.top(len)),
-                Instruction::CopyReturnToData => self.data_st.push(self.return_st.top(len)),
-                Instruction::CopyReturnToSwap => self.swap_st.push(self.return_st.top(len)),
-                Instruction::CopyReturnToReturn => self.return_st.duplicate(len),
-                Instruction::CopyReturnToHold => self.hold_reg.push(self.return_st.top(len)),
-                Instruction::CopyHoldToData => self.data_st.push(self.hold_reg.top(len)),
-                Instruction::CopyHoldToSwap => self.swap_st.push(self.hold_reg.top(len)),
-                Instruction::CopyHoldToReturn => self.return_st.push(self.hold_reg.top(len)),
-                Instruction::Literal => {
-                    let lit_range = self.get_lit_range(len);
-                    self.data_st.push(&self.memory[lit_range]);
-                    self.program_counter += 1 + len as u32;
-                    continue; // avoid default PC increment
-                }
-
-                // branching
-                Instruction::Jump {
-                    conditional,
-                    relative,
-                } => {
-                    if conditional {
-                        let condition = self.data_st.top_byte();
-                        self.data_st.drop(1);
-                        if condition == STACK_FALSE {
-                            continue; // don't execute the jump
-                        };
-                    }
-
-                    let value = CPU::le_slice_to_u32(self.data_st.top(len));
-                    self.data_st.drop(len);
-
-                    self.program_counter = match relative {
-                        true => self.program_counter + value,
-                        false => value,
-                    };
-
-                    continue; // avoid default PC increment
-                }
-                Instruction::Call => {
-                    self.return_st.push(&self.program_counter.to_le_bytes());
-                    self.program_counter = CPU::le_slice_to_u32(self.data_st.top(len));
-                    self.data_st.drop(len);
-                    continue; // avoid default PC increment
-                }
-                Instruction::Return => {
-                    self.program_counter = CPU::le_slice_to_u32(self.return_st.top(len));
-                    self.return_st.drop(len);
-                    continue; // avoid default PC increment
-                }
-
-                // accessing memory
-                Instruction::Address => {
-                    self.memory_address = CPU::le_slice_to_u32(self.data_st.top(len));
-                }
-                Instruction::Store => {
-                    let data = self.data_st.top(len);
-                    let range = self.memory_address as usize..self.memory_address as usize + len;
-                    self.memory[range].copy_from_slice(data);
-                }
-                Instruction::Load => {
-                    let range = self.memory_address as usize..self.memory_address as usize + len;
-                    let data = &self.memory[range];
-                    self.data_st.push(data);
-                }
-
-                // dropping stacks
-                Instruction::DropData => self.data_st.drop(len),
-                Instruction::DropSwap => self.swap_st.drop(len),
-                Instruction::DropReturn => self.return_st.drop(len),
-
-                // arithmetic
-                Instruction::Add => {
-                    let (lhs, rhs) = self.pop_operands(len);
-                    self.push_result(len, lhs + rhs);
-                }
-                Instruction::Subtract => {
-                    let (lhs, rhs) = self.pop_operands(len);
-                    self.push_result(len, lhs - rhs);
-                }
-                Instruction::Multiply => {
-                    let (lhs, rhs) = self.pop_operands(len);
-                    self.push_result(len, lhs * rhs);
-                }
-                Instruction::Divide => {
-                    let (lhs, rhs) = self.pop_operands(len);
-                    self.push_result(len, lhs / rhs);
-                }
-
-                // bitwise logic
-                Instruction::And => {
-                    let (lhs, rhs) = self.pop_operands(len);
-                    self.push_result(len, lhs & rhs);
-                }
-                Instruction::Or => {
-                    let (lhs, rhs) = self.pop_operands(len);
-                    self.push_result(len, lhs | rhs);
-                }
-                Instruction::Xor => {
-                    let (lhs, rhs) = self.pop_operands(len);
-                    self.push_result(len, lhs ^ rhs);
-                }
-                Instruction::Not => {
-                    let operand = self.pop_operand(len);
-                    self.push_result(len, !operand);
-                }
-                Instruction::ShiftL => {
-                    let (shift, operand) = self.pop_operands(len);
-                    self.push_result(len, operand << shift);
-                }
-                Instruction::ShiftR => {
-                    let (shift, operand) = self.pop_operands(len);
-                    self.push_result(len, operand >> shift);
-                }
-
-                // comparisons
-                Instruction::Greater => {
-                    let (lhs, rhs) = self.pop_operands(len);
-                    self.push_bool_result(lhs > rhs);
-                }
-                Instruction::Less => {
-                    let (lhs, rhs) = self.pop_operands(len);
-                    self.push_bool_result(lhs < rhs);
-                }
-                Instruction::Equal => {
-                    let (lhs, rhs) = self.pop_operands(len);
-                    self.push_bool_result(lhs == rhs);
-                }
-                Instruction::NotEqual => {
-                    let (lhs, rhs) = self.pop_operands(len);
-                    self.push_bool_result(lhs != rhs);
-                }
-            }
-
-            self.program_counter += 1;
+            self.execute()
         }
     }
 
@@ -335,21 +373,40 @@ impl CPU {
         start..end
     }
 
-    fn pop_operand(&mut self, len: usize) -> u64 {
+    fn pop_operand8(&mut self) -> u8 {
+        let operand = self.data_st.top_byte();
+        self.data_st.drop(1);
+        operand
+    }
+
+    fn pop_operand32(&mut self, len: usize) -> u32 {
+        let operand = CPU::le_slice_to_u32(self.data_st.top(len));
+        self.data_st.drop(len);
+        operand
+    }
+    fn pop_operands32(&mut self, len: usize) -> (u32, u32) {
+        (self.pop_operand32(len), self.pop_operand32(len))
+    }
+
+    fn pop_operand64(&mut self, len: usize) -> u64 {
         let operand = CPU::le_slice_to_u64(self.data_st.top(len));
         self.data_st.drop(len);
         operand
     }
-    fn pop_operands(&mut self, len: usize) -> (u64, u64) {
-        (self.pop_operand(len), self.pop_operand(len))
+    fn pop_operands64(&mut self, len: usize) -> (u64, u64) {
+        (self.pop_operand64(len), self.pop_operand64(len))
     }
-    fn push_result(&mut self, len: usize, result: u64) {
-        self.data_st.push(&result.to_le_bytes()[0..len]);
-    }
-    fn push_bool_result(&mut self, result: bool) {
+
+    fn push_result_bool(&mut self, result: bool) {
         self.data_st.push_byte(match result {
             true => 0xff,
             false => STACK_FALSE,
         });
+    }
+    fn push_result8(&mut self, result: u8) {
+        self.data_st.push_byte(result);
+    }
+    fn push_result64(&mut self, len: usize, result: u64) {
+        self.data_st.push(&result.to_le_bytes()[0..len]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,17 @@ pub(crate) mod core;
 
 fn main() {
     let mut cpu = core::CPU::new();
-    cpu.run();
+    loop {
+        cpu.execute();
+        check_dma(&cpu.dma_controller, &cpu.memory);
+    }
+}
+
+fn check_dma(mut dma: &core::DMA, memory: &[u8; 65_536]) {
+    let has_request = core::DMA::REQUEST_BIT & dma.status_reg != 0;
+    if has_request {
+        // process request
+        let payload_range = dma.address as usize..(dma.address + dma.length) as usize;
+        let payload = &memory[payload_range];
+    }
 }
