231aa37766ea8b57cece340ca3fcd41a81e4ae70
Jak Tiano <jak33@mac.com>
2022-12-20 00:03:41 -0500

    fixed issue when tabs weren't at start of line

--- src/assembler/parsing.rs
+++ src/assembler/parsing.rs
@@ -1,8 +1,9 @@
 use crate::assembler::tokens::COMMENT_CLOSE;
 use crate::assembler::tokens::{Rune, TextToken};
 use core::str::Chars;
+use std::iter::Peekable;
 
-pub fn string_until(close_char: char, chars: &mut Chars) -> Result<String, String> {
+pub fn string_until(close_char: char, chars: &mut Peekable<Chars>) -> Result<String, String> {
     let mut found = false;
     let mut string = String::new();
     while let Some(c) = chars.next() {
@@ -35,45 +36,40 @@ pub fn parse_text(assembly_text: &str) -> Result<Vec<TextToken>, String> {
 
     let mut text_tokens = Vec::new();
     let mut buffer = String::new();
-    let mut chars = assembly_text.chars();
+    let mut chars = assembly_text.chars().peekable();
     while let Some(c) = chars.next() {
         match c {
             '\n' => {
                 handle_token(&mut buffer, &mut text_tokens)?;
                 text_tokens.push(TextToken::NewLine);
-            }
-            '\t' => {
-                handle_token(&mut buffer, &mut text_tokens)?;
-                let mut num_tabs = 1;
-                loop {
-                    match chars.next() {
-                        Some(c) => match c {
-                            '\t' => num_tabs += 1,
-                            c => {
-                                buffer.push(c);
-                                break;
-                            }
-                        },
-                        None => break,
+
+                // look for indentation after a new line
+                let mut num_tabs = 0;
+                while let Some(c) = chars.peek() {
+                    match c {
+                        '\t' => {
+                            num_tabs += 1;
+                            chars.next();
+                        }
+                        _ => break,
                     }
                 }
-
-                text_tokens.push(TextToken::Tab(num_tabs));
+                if num_tabs > 0 {
+                    text_tokens.push(TextToken::Tab(num_tabs));
+                }
             }
             c if c.is_whitespace() => {
                 handle_token(&mut buffer, &mut text_tokens)?;
-                if let Some(last) = text_tokens.last() {
-                    if let TextToken::Rune(rune) = last {
-                        match rune {
-                            &Rune::OpenComment => {
-                                text_tokens.pop();
-                                let comment = string_until(COMMENT_CLOSE, &mut chars)?;
-                                let comment = comment.trim().to_string();
-                                text_tokens.push(TextToken::Comment(comment));
-                            }
-                            _ => {}
-                        }
-                    }
+
+                // if we just added a TextToken::Rune(Rune::OpenComment)
+                if matches!(text_tokens.last(), Some(TextToken::Rune(Rune::OpenComment))) {
+                    // pop the TextToken::Rune
+                    text_tokens.pop();
+
+                    // replace with parsed TextToken::Comment
+                    let comment = string_until(COMMENT_CLOSE, &mut chars)?;
+                    let comment = comment.trim().to_string();
+                    text_tokens.push(TextToken::Comment(comment));
                 }
             }
             c => buffer.push(c),
